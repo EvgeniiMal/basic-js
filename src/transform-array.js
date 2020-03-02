093601e05a441f7391ed7d54c072fbc0dd5debf3
EvgeniiMal <eugenemal88@gmail.com>
2020-03-02 16:38:14 +0200

    refactor: change controlFunction call method

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,23 +4,23 @@ module.exports = function transform(arr) {
     let controller = {
         '--double-next': (index) => {
             arr[index + 1] !== undefined ? arr[index] = arr[index + 1] : arr.splice(index, 1);
-         },
+        },
         '--double-prev': (index) => {
             arr[index - 1] !== undefined ? arr[index] = arr[index - 1] : arr.splice(index, 1);
         },
         '--discard-next': (index) => {
             arr[index + 1] !== undefined ? arr.splice(index, 2) : arr.splice(index, 1);
         },
-        '--discard-prev':  (index) => {
-            arr[index - 1] !== undefined ? arr.splice(index - 1, 2) : arr.splice(index, 1); 
+        '--discard-prev': (index) => {
+            arr[index - 1] !== undefined ? arr.splice(index - 1, 2) : arr.splice(index, 1);
         }
     };
 
     for (let [flag, controlFunction] of Object.entries(controller)) {
-       if(arr.indexOf(flag) != -1){
-        controlFunction.call(this, arr.indexOf(flag));
-        transform(arr);
-       }
+        if (arr.indexOf(flag) != -1) {
+            controlFunction(arr.indexOf(flag));
+            transform(arr);
+        }
     }
 
     return arr;
