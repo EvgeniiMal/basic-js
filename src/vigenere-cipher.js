9d9efeb27c53dcba7f63089f98beaf21e5a735da
EvgeniiMal <eugenemal88@gmail.com>
2020-03-05 03:42:54 +0200

    feat: add 'vigenere' solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,57 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct = true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    buildKey(messageArr, key) {
+        let keyArr = [];
+        let keyCharIndex = 0;
+        for (let i = 0; i < messageArr.length; i++) {
+            if (messageArr[i].charCodeAt() < 65 || messageArr[i].charCodeAt() > 90) {
+                keyArr.push(messageArr[i]);
+
+            } else {
+                keyArr.push(key[keyCharIndex]);
+                keyCharIndex++;
+            }
+            if (keyCharIndex == key.length) {
+                keyCharIndex = 0;
+            }
+        }
+        return keyArr;
+    }
+    encrypt(message, key) {
+        if (!message || !key) throw new Error()
+        message = message.toUpperCase().split('');
+        key = this.buildKey(message, key.toUpperCase())
+        let result = []; 
+
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+                result.push(message[i])
+            } else {
+                let char = (message[i].charCodeAt() - 65 + key[i].charCodeAt() - 65) % 26 + 65;
+                result.push(String.fromCharCode(char))
+            }
+        }
+        return this.direct ? result.join('') : result.reverse().join('');
+    }
+
+    decrypt(message, key) {
+        if (!message || !key) throw new Error()
+        message = message.toUpperCase().split('');
+        key = this.buildKey(message, key.toUpperCase())
+        let result = [];
+
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+                result.push(message[i])
+            } else {
+                let char = ((message[i].charCodeAt() - 65) + 26 - (key[i].charCodeAt() - 65)) % 26 + 65;
+                result.push(String.fromCharCode(char))
+            }
+        }
+        return this.direct ? result.join('') : result.reverse().join('');
     }
 }
 
